eb0e1c99b51e3278f4e851407b00155aa1431e8b
yoohyebin <hyebin218@naver.com>
2025-08-27 15:44:47 +0900

    init: swagger 추가

---
+++ src/main/java/com/example/popin/global/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.popin.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authz -> authz
+                    .antMatchers(
+                            "/swagger-ui/**",
+                            "/swagger-ui.html",
+                            "/v3/api-docs/**",
+                            "/v3/api-docs"
+                    ).permitAll()
+                    .anyRequest().authenticated()
+            )
+            .csrf().disable();
+
+        return http.build();
+    }
+}
