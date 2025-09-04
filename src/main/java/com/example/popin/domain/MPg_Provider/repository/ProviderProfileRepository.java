6d0da36a61ed37a893211438e820b3b2725dcb52
beakjinu <dighwlsdn96@naver.com>
2025-09-04 11:35:44 +0900

    refactor: user 엔티티 경로 변경 및 repository변경으로 인한 기타 수정

--- src/main/java/com/example/popin/domain/MPg_Provider/repository/ProviderProfileRepository.java
+++ src/main/java/com/example/popin/domain/MPg_Provider/repository/ProviderProfileRepository.java
@@ -2,7 +2,7 @@ package com.example.popin.domain.MPg_Provider.repository;
 
 import com.example.popin.domain.MPg_Provider.entity.ProviderProfile;
 import com.example.popin.domain.space.entity.Space;
-import com.example.popin.domain.user.User;
+import com.example.popin.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
