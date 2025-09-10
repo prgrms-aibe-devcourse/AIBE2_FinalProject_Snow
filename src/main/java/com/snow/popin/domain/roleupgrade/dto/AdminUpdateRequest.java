22504388a4ad5f97f3be629c73357d5262cc9051
dearjh36 <dearjh36@naver.com>
2025-09-10 15:13:56 +0900

    feat: 역할 승격 요청 DTO/엔티티 추가

---
+++ src/main/java/com/snow/popin/domain/roleupgrade/dto/AdminUpdateRequest.java
@@ -0,0 +1,39 @@
+package com.snow.popin.domain.roleupgrade.dto;
+
+import com.snow.popin.domain.roleupgrade.entity.ApprovalStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class AdminUpdateRequest {
+
+    @NotBlank(message = "승인 상태는 필수입니다")
+    private Boolean approve;
+
+    private String rejectReason; // 반려 시에만 필요
+
+    @Builder
+    public AdminUpdateRequest(Boolean approve, String rejectReason) {
+        this.approve = approve;
+        this.rejectReason = rejectReason;
+
+    }
+
+    public boolean isApprove(){
+        return approve != null && approve;
+    }
+
+    // 반려 시 반려 사유 필수
+    @AssertTrue(message = "반려 시 반려 사유를 입력해주세요.")
+    private boolean isRejectReasonValid(){
+        if (approve != null && !approve){
+            return rejectReason != null && !rejectReason.trim().isEmpty();
+        }
+        return true;
+    }
+}
