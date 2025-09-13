074ab6248bffd6ed03a26c39dc2b08a25ec2ef7b
dearjh36 <dearjh36@naver.com>
2025-09-13 13:53:53 +0900

    feat: 어드민 유저 관리용 DTO 및 Enum 추가 및 기존 유저 수정

--- src/main/java/com/snow/popin/domain/user/repository/UserRepository.java
+++ src/main/java/com/snow/popin/domain/user/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.snow.popin.domain.user.repository;
 
 import com.snow.popin.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> , JpaSpecificationExecutor<User> {
     // 이메일로 사용자 조회
     Optional<User> findByEmail(String email);
 
@@ -16,4 +20,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByNickname(String nickname);
 
+    Page<User> findAll(Specification<User> spec, Pageable pageable);
 }
