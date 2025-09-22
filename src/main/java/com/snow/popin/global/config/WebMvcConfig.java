7a99079f2932bc613036886a0b95d7bfce3b0b70
dearjh36 <dearjh36@naver.com>
2025-09-22 21:46:14 +0900

    fix: 인코딩 설정 추가

--- src/main/java/com/snow/popin/global/config/WebMvcConfig.java
+++ src/main/java/com/snow/popin/global/config/WebMvcConfig.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.ContentNegotiationConfi
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
