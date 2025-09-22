e0eca591374ae4169516092d27d694940d5f1cb7
dearjh36 <dearjh36@naver.com>
2025-09-22 22:04:51 +0900

    fix: main 원래대로 바꿈

--- src/main/java/com/snow/popin/global/controller/MainController.java
+++ src/main/java/com/snow/popin/global/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    /*
     @GetMapping("/")
     public String index() {
         return "forward:/templates/pages/index.html"; // 기존 home.html 활용
@@ -16,4 +17,6 @@ public class MainController {
     public String main() {
         return "forward:/templates/pages/index.html";
     }
+    */
+     
 }
