d209369ea43deab33f412d41f8b285d0418f3ab2
dearjh36 <dearjh36@naver.com>
2025-09-23 13:23:27 +0900

    fix: mainController 활성화

--- src/main/java/com/snow/popin/global/controller/MainController.java
+++ src/main/java/com/snow/popin/global/controller/MainController.java
@@ -7,16 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    /*
     @GetMapping("/")
     public String index() {
-        return "forward:/templates/pages/index.html"; // 기존 home.html 활용
+        return "forward:/index.html"; // static 폴더의 index.html로 포워드
     }
 
     @GetMapping("/main")
     public String main() {
-        return "forward:/templates/pages/index.html";
+        return "forward:/index.html";
     }
-    */
+
+    @GetMapping("/home")
+    public String home() {
+        return "forward:/home.html"; // static 폴더의 home.html로 포워드
+    }
      
 }
